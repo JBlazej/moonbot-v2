a66c2c20f8bfb3fcb279aa17a50754c6086d075b
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-22 00:58:22 +0100

    smazani fix

--- src/app.js
+++ src/app.js
@@ -63,6 +63,7 @@ app.use((req, res, next) => {
   
 app.use((err, req, res, next) => {
     res.status(err.status || 500)
+    console.log(err)
     res.send({'error': err.message})
     if (app.get('env') === 'development') {
       logger.error(err)
