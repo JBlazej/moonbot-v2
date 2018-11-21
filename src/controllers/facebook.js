c21dfef39e7bfab4ffc47322c88b0ec15f1ed6e7
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-21 21:34:51 +0100

    debug

--- src/controllers/facebook.js
+++ src/controllers/facebook.js
@@ -10,16 +10,10 @@ export async function  facebookVerificationHook(req, res) {
      
    // Checks if a token and mode is in the query string of the request
    if (mode && token) {
-   
-     // Checks the mode and token sent is correct
      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-       
-       // Responds with the challenge token from the request
-       console.log('WEBHOOK_VERIFIED')
+         
        res.status(200).send(challenge)
-     
      } else {
-       // Responds with '403 Forbidden' if verify tokens do not match
        res.sendStatus(403);    
      }
    }
@@ -28,21 +22,16 @@ export async function  facebookVerificationHook(req, res) {
 export async function facebookEventHook(req, res){
     let body = req.body
 
-    // Checks this is an event from a page subscription
     if (body.object === 'page') {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
+            let webhook_event = entry.messaging[0]
             
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-        
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
+            console.log(webhook_event)
+            console.log(entry)
+        })
+        res.status(200).send('EVENT_RECEIVED')
     } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+        res.sendStatus(404)
     }
 }
