0a776c8ee4159114e434663f969af5fd849b025a
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-22 13:36:59 +0100

    payload

--- src/bot/payloads/payloads.js
+++ src/bot/payloads/payloads.js
@@ -10,7 +10,7 @@ export async function payloads(id, payload){
         const msg2 = 'Jak se máš? Já se mám dneska fakt skvěle. Konečně je tu někdo komu můžu pomoct'
         const msg3 = 'Napiš příkaz moon a já ti o sobě řeknu více'
 
-        await sendTextMessage(id, msg1).then(()=>{sendTextMessage(webhookEvent.sender.id, msg2)}).then(()=>{sendTextMessage(webhookEvent.sender.id, msg3)})
+        await sendTextMessage(id, msg1).then(()=>{sendTextMessage(id, msg2)}).then(()=>{sendTextMessage(id, msg3)})
         break
     }
 }
