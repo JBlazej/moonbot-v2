0ee707b750bb5068298a1869f73249f7e906b8d1
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-24 19:45:07 +0100

    fix

--- src/bot/middleware.js
+++ src/bot/middleware.js
@@ -4,14 +4,13 @@ import {payloads} from './payloads'
 export async function botMessengerMiddleware(event) {
   let webhookEvent = event
   
-  if(webhookEvent.message.text) {
+  if(webhookEvent.message) {
     // Text
     commands(webhookEvent)
-  }else if(webhookEvent.message.attachments) {
-    // Attachments
-    console.log('Prisel')
   }else {
     // Payload
     payloads(webhookEvent.sender.id, webhookEvent.postback.payload)
   }
 }
+
+//webhookEvent.message.attachments
