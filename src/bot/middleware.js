91032727ccdf0e188f9d647f463f1e2a890061a2
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-24 19:47:40 +0100

    fix

--- src/bot/middleware.js
+++ src/bot/middleware.js
@@ -5,8 +5,12 @@ export async function botMessengerMiddleware(event) {
   let webhookEvent = event
   
   if(webhookEvent.message) {
-    // Text
-    commands(webhookEvent)
+    if(webhookEvent.message.text) {
+      // Text
+      commands(webhookEvent)
+    }else {
+      console.log('Nejke hovno')
+    }
   }else {
     // Payload
     payloads(webhookEvent.sender.id, webhookEvent.postback.payload)
