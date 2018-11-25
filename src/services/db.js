1db86c3c2c6595de15fd57f77acbc9463079e960
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-25 05:08:34 +0100

    new arch

--- src/services/db.js
+++ src/services/db.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose'
 import {User} from '../models/v1/user'
 
 export class UserService {
+    constructor(
+        
+    )
     
     async setHackerNews(id, param){
         let data = {
