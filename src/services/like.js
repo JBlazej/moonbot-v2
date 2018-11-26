34db451fcd8e562e202905f5a0cabe042a62977c
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-26 01:43:06 +0100

    like

--- src/services/like.js
+++ src/services/like.js
@@ -3,7 +3,7 @@ import {Like} from '../models/v1/like'
 
 export async function createNewLikeInstance (setYear, setLike) {
     const like = new Like({year : setYear, like : setLike})
-
+    console.log(like)
     like.save()
     
     console.log(like)
