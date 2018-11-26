cbb68b3c9daa8061f1f5953b00ceb75e841fcc79
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-26 21:05:11 +0100

    fix google auth

--- src/bot/lib/dateAndTime.js
+++ src/bot/lib/dateAndTime.js
@@ -63,7 +63,7 @@ export function increaseTime(utcTimeAndDate){
 }
 
 function getByHourIdosSettings(utcTimeAndDate, a){
-    const hour = moment(utcTimeAndDate).format('hh')
+    const hour = moment(utcTimeAndDate).format('H')
     const set = incrementTimeMinutes
     console.log(hour)
     if (hour === 0) {
