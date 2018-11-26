9629f4b2bf2b70665a7bba25431310fffe6546d4
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-26 01:44:07 +0100

    like

--- src/services/like.js
+++ src/services/like.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import {Like} from '../models/v1/like'
 
 export async function createNewLikeInstance (setYear, setLike) {
-    const like = new Like({year : setYear, like : setLike})
+    const like = new Like({year : setYear, likes : setLike})
     console.log(like)
     like.save()
     
