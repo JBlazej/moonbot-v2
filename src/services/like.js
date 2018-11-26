a5983fde75728582b04de211a4f2529cbacb81b3
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-26 02:11:39 +0100

    like

--- src/services/like.js
+++ src/services/like.js
@@ -11,17 +11,30 @@ export async function createNewLikeInstance (setYear, setLike) {
 
 export async function incrementLikeInstance (year, set){
     const like = await getLikeInstance(year)
+    
     like[0].likes = like[0].likes + set
 
     console.log(typeof like[0].likes)
     console.log(like[0].likes)
 
-    return like
+    const newLikeNumber = await updateLikeInstance(year, like[0].likes)
+
+    return newLikeNumber
 }
 
 
 export async function getLikeInstance (year) {
     const like = Like.find({"year": year})
+    
+    return like
+}
+
+export async function updateLikeInstance (year, like) {
+    let data = {
+        likes : like
+    }
+
+    const like = Like.findOneAndUpdate({year : year}, data, {new: true})
     
     return like
 }
