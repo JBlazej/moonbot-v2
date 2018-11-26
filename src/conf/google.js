47b223c98e097c5a045e8879a977e3e01a61a3f7
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-26 21:28:06 +0100

    lang

--- src/conf/google.js
+++ src/conf/google.js
@@ -8,13 +8,13 @@ let CLIENT_X509_URL = process.env.CLIENT_X509_URL
 
 export const gooAuth = {
     type: "service_account",
-    project_id: PROJECT_ID,
-    private_key_id: PRIVATE_KEY_ID,
-    private_key: PRIVATE_KEY,
-    client_email: CLIENT_EMAIL,
-    client_id: CLIENT_ID,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: CLIENT_X509_URL
+    client_x509_cert_url: process.env.CLIENT_X509_URL
   }
