b28f2273260db1a0c0181a7db407e843d0a4bdaf
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-27 19:44:56 +0100

    cron

--- src/services/sigterm.js
+++ src/services/sigterm.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import request from 'request'
 
 export async function sigterm () {
-  cron.schedule('*/10 * * * *', () => {
+  cron.schedule('*/1 * * * *', () => {
     request('https://moonbot-v2.herokuapp.com')
   })
 }
