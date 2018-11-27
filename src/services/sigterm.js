64e472870a7c41d7472b6b6a79ff003ba1c15138
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-27 17:15:17 +0100

    cron

--- src/services/sigterm.js
+++ src/services/sigterm.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import request from 'request'
 
 export async function sigterm () {
-  cron.schedule('*/10 * * * *', () => {
+  cron.schedule('*/7 * * * *', () => {
     request('https://moonbot-v3.herokuapp.com', { json: false }, (err, res) => {
       if (err) { return console.log(err) }
       console.log('Ping and now you will not died!!')
