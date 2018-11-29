d3872dc1d23e8b09f7fa81de1136409edf31c1ec
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-29 13:43:50 +0100

    test

--- src/services/redis.js
+++ src/services/redis.js
@@ -9,7 +9,7 @@ const REDIS_URL = process.env.REDIS_URL;
 
 
 var eventNotifier = new RedisNotifier(redis, {
-    redis : { host : 'redis://h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com', port : 37959 },
+    redis : { host : 'h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com', port : 37959 },
     expired : true,
     evicted : true,
     logLevel : 'DEBUG' //Defaults To INFO
