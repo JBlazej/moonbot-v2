a303d5d2d6714bce36efe76decbf454c9fb4d61d
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-29 13:41:54 +0100

    test

--- src/services/redis.js
+++ src/services/redis.js
@@ -6,43 +6,14 @@ import redis from 'redis'
 import RedisNotifier from 'redis-notifier'
 
 const REDIS_URL = process.env.REDIS_URL;
-const client = redis.createClient(REDIS_URL);
 
-client.on('connect', runSample)
 
-client.on('error', err => {
-    console.log(`Error: ${err}`);
-})
-
-function runSample() {
-    // Set a value
-    client.set('string key', 'Hello World', redis.print);
-    // Expire in 3 seconds
-    client.expire('string key', 3);
- 
-    // This timer is only to demo the TTL
-    // Runs every second until the timeout
-    // occurs on the value
-    var myTimer = setInterval(function() {
-        client.get('string key', function (err, reply) {
-            if(reply) {
-                console.log('I live: ' + reply.toString());
-                client.ttl('string key', writeTTL);
-            } else {
-                clearTimeout(myTimer);
-                console.log('I expired');
-                client.quit();
-            }
-        });
-    }, 1000);
-}
- 
-function writeTTL(err, data) {
-    console.log('I live for this long yet: ' + data);
-    
-}
-
-var eventNotifier = new RedisNotifier(redis, REDIS_URL);
+var eventNotifier = new RedisNotifier(redis, {
+    redis : { host : 'redis://h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com', port : 37959 },
+    expired : true,
+    evicted : true,
+    logLevel : 'DEBUG' //Defaults To INFO
+  });
   
   //Listen for event emission
   eventNotifier.on('message', function(pattern, channelPattern, emittedKey) {
@@ -50,7 +21,6 @@ var eventNotifier = new RedisNotifier(redis, REDIS_URL);
     switch(channel.key) {
       case 'expired':
         console.info(`Expired Key ${key}`);
-        console.log('expired jak cyp')
         break;
       case "evicted":
         console.info(`Evicted Key ${key}`);        
@@ -59,3 +29,6 @@ var eventNotifier = new RedisNotifier(redis, REDIS_URL);
         logger.debug("Unrecognized Channel Type:" + channel.type);
     }
   });
+
+
+  // redis://h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com:37959
