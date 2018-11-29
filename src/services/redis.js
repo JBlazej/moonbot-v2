5dd37cd30d042efd4424e732b479f9774f7dfb2c
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-29 13:32:59 +0100

    test

--- src/services/redis.js
+++ src/services/redis.js
@@ -3,6 +3,7 @@
  * Test
  */
 import redis from 'redis'
+import RedisNotifier from 'redis-notifier'
 
 const REDIS_URL = process.env.REDIS_URL;
 const client = redis.createClient(REDIS_URL);
@@ -40,3 +41,26 @@ function writeTTL(err, data) {
     console.log('I live for this long yet: ' + data);
     
 }
+
+var eventNotifier = new RedisNotifier(redis, {
+    redis : REDIS_URL,
+    expired : true,
+    evicted : true,
+    logLevel : 'DEBUG' //Defaults To INFO
+});
+  
+  //Listen for event emission
+  eventNotifier.on('message', function(pattern, channelPattern, emittedKey) {
+    var channel = this.parseMessageChannel(channelPattern);
+    switch(channel.key) {
+      case 'expired':
+        console.info(`Expired Key ${key}`);
+        console.log('expired jak cyp')
+        break;
+      case "evicted":
+        console.info(`Evicted Key ${key}`);        
+        break;
+      default:
+        logger.debug("Unrecognized Channel Type:" + channel.type);
+    }
+  });
