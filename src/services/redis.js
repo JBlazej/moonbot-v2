fde8cd1d92f31b841df31091fa90adf2668437c7
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-29 13:45:46 +0100

    test

--- src/services/redis.js
+++ src/services/redis.js
@@ -6,14 +6,11 @@ import redis from 'redis'
 import RedisNotifier from 'redis-notifier'
 
 const REDIS_URL = process.env.REDIS_URL;
+const client = redis.createClient(REDIS_URL);
 
 
-var eventNotifier = new RedisNotifier(redis, {
-    redis : { host : 'h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com', port : 37959 },
-    expired : true,
-    evicted : true,
-    logLevel : 'DEBUG' //Defaults To INFO
-  });
+
+var eventNotifier = new RedisNotifier(client);
   
   //Listen for event emission
   eventNotifier.on('message', function(pattern, channelPattern, emittedKey) {
@@ -29,6 +26,4 @@ var eventNotifier = new RedisNotifier(redis, {
         logger.debug("Unrecognized Channel Type:" + channel.type);
     }
   });
-
-
-  // redis://h:p142b6c695224059ec6732cfe7f19b6f6466875675b9f449dec6e9b26ce5e7361@ec2-63-32-66-219.eu-west-1.compute.amazonaws.com:37959
+  
