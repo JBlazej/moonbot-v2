ffe12dbc4e995eeedf7fedf39311ec0af0a69c05
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-29 13:54:04 +0100

    test

--- src/services/redis.js
+++ src/services/redis.js
@@ -6,12 +6,12 @@ import redis from 'redis'
 import RedisNotifier from 'redis-notifier'
 
 const REDIS_URL = process.env.REDIS_URL;
-const client = redis.createClient(REDIS_URL);
+//const client = redis.createClient(REDIS_URL);
 
 
 
 var eventNotifier = new RedisNotifier(redis, {
-    redis : { host : 'ec2-63-32-66-219.eu-west-1.compute.amazonaws.com', port : 37959 },
+    redis : { url: REDIS_URL },
     expired : true,
     evicted : true,
     logLevel : 'DEBUG' //Defaults To INFO
