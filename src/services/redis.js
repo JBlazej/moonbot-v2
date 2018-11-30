d8f8b6cf5da369938a35773253a790f388976d8e
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-30 22:04:53 +0100

    redis test 2

--- src/services/redis.js
+++ src/services/redis.js
@@ -2,35 +2,20 @@
  * Redis
  * Test
  */
-import redis from 'redis'
+import Redis from 'ioredis'
 
 const REDIS_URL = process.env.REDIS_URL;
-//const client = redis.createClient(REDIS_URL);
+const redis = new Redis(REDIS_URL)
 
-const client = redis.createClient(REDIS_URL)
-
-client.on("error", function (err) {
-  console.log("Error " + err);
-});
-
-client.on("connect", runSample);
-
-function runSample() {
-
-  console.log(client)
-    client.set('string key', 'Hello World', redis.print);
-
-    client.expire('string key', 3);
- 
-    var myTimer = setInterval(function() {
-        client.get('string key', function (err, reply) {
-            if(reply) {
-                console.log('I live: ' + reply.toString());
-            } else {
-                clearTimeout(myTimer);
-                console.log('I expired');
-                client.quit();
-            }
-        });
-    }, 1000);
+async function main () {
+  try{
+    await redis.set('cat', 'Charlie')
+  } catch (error) {
+    console.log(error)
+  }
+  redis.disconnect()
 }
+
+redis.connect(() => console.log('Connected'))
+
+main()
