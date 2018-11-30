2939edc60b0cf34076387d73ab5e25e6d028ed89
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-30 22:33:42 +0100

    redis test 2

--- src/services/publisher.js
+++ src/services/publisher.js
@@ -11,10 +11,9 @@ async function main() {
     // Sleep 4 seconds and then publish garage door "opened" event.
     await sleep(4);
     pub.publish(channel, 'opened');
-
-    await sleep(7);
+    await sleep(4);
     pub.publish(channel, 'closed');
-    pub.disconnect();
+
 }
 
 main();
