411168dcca1842e466e8cf08fb87370f1bd62c4e
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-12-01 14:30:11 +0100

    redis test 2

--- src/services/subscriber.js
+++ src/services/subscriber.js
@@ -17,3 +17,29 @@ redis.subscribe(channel, (error, count) => {
 });
 
 redis.subscribe("__keyevent@0__:expired")
+
+redis.on('pmessage', function(pattern,channel, msg) {
+    console.log( "S2: received on "+channel+" event "+msg )
+});
+redis.psubscribe( "__keyspace@0__:*", function (err) {
+    next();
+});
+
+function start () {
+    redis.set('cat', 'Garfield');
+    redis.expire('cat', 8)
+    
+    var myTimer = setInterval(function() {
+    redis.get('cat', function (err, reply) {
+        if(reply) {
+            console.log('I live: ' + reply.toString());
+        } else {
+            clearTimeout(myTimer);
+            console.log('I expired');
+            client.quit();
+        }
+    });
+    }, 1000);
+}
+
+start()
