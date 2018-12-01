4199ab845f3b705edfedebda9448c47def54beae
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-12-01 14:45:15 +0100

    redis test 3

--- src/services/subscriber.js
+++ src/services/subscriber.js
@@ -2,7 +2,7 @@ import Redis from 'ioredis'
 
 const redis = new Redis(process.env.REDIS_URL)
 
-redis.on('message', (channel, message) => {
+/**redis.on('message', (channel, message) => {
     console.log(`Received the following message from ${channel}: ${message}`);
 });
 
@@ -15,6 +15,7 @@ redis.subscribe(channel, (error, count) => {
     }
     console.log(`Subscribed to ${count} channel. Listening for updates on the ${channel} channel.`);
 });
+ */
 
 //redis.subscribe("__keyevent@0__:expired")
 
