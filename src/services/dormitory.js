8691a2fe7d54d8a5349c857fcdd3a30d8741fddb
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-01-07 23:59:00 +0100

    createdorm

--- src/services/dormitory.js
+++ src/services/dormitory.js
@@ -2,7 +2,7 @@ import {Dormitory} from '../models/v1/dormitory'
 
 
 
-export async function createDormitory (id, title, headName, headEmail, headPhone, repName, repEmail, headPhone, adressURL) {
+export async function createDormitory (id, title, headName, headEmail, headPhone, repName, repEmail, repPhone, adressURL) {
     const dormitory = new Dormitory({
         id: id, 
         title: title, 
@@ -15,7 +15,7 @@ export async function createDormitory (id, title, headName, headEmail, headPhone
             representative: {
                 name: repName, 
                 email: repEmail, 
-                phone: headPhone
+                phone: repPhone
             }
         },
         adressURL: adressURL
