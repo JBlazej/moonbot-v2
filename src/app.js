2c85682c7ad68e712c79605d2af5ea978949e333
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-01-08 00:16:22 +0100

    dormitory

--- src/app.js
+++ src/app.js
@@ -94,5 +94,7 @@ app.use((err, req, res, next) => {
     }
 })
 
+import { sendHeadAndRep } from './bot/vse'
+sendHeadAndRep('1')
 //createDormitory('1', 'Blanice', 'Miroslava Tichá', 'tichami@vse.cz', '+420 224 092 511', 'Petra Kowalská', 'petra.kowalska@vse.cz', '+420 224 092 513', 'https://www.google.com/maps/place/Kolej+Blanice/@50.0198007,14.4956339,17z/data=!3m1!4b1!4m5!3m4!1s0x470b9229c94d6f2b:0x3427c9d8147fef0f!8m2!3d50.0197973!4d14.4978227')
 export default app
