bc1441ce242ae4b027a6c7d8eede216f7a22cbef
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-01-08 00:19:08 +0100

    dormitory

--- src/bot/vse/index.js
+++ src/bot/vse/index.js
@@ -4,6 +4,7 @@ import { getDormitoryById } from '../../services/dormitory'
 export async function sendHeadAndRep (id) {
     let data = await getDormitoryById(id)
 
-    console.log(data)
-    console.log(typeof data)
+    console.log(data[0].contact.head[0].name)
+    console.log(data[0].contact.head[0].email)
+    console.log(data[0].contact.head[0].phone)
 }
