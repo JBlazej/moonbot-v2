9828fb2aad7d2086e9edf4c126a233801e5a49fb
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-01-08 00:35:47 +0100

    dormitory vedouci a repre

--- src/bot/vse/index.js
+++ src/bot/vse/index.js
@@ -1,21 +1,13 @@
-import { sendMultipleMessages } from '../lib/messages'
+import { sendTextMessage, sendMultipleMessages } from '../lib/messages'
 import { getDormitoryById } from '../../services/dormitory'
 
 export async function sendHeadAndRep(id, sender) {
     let data = await getDormitoryById(id)
-    let head = [
-        data[0].contact.head.name,
-        data[0].contact.head.email,
-        data[0].contact.head.phone
-    ]
+    
+    let head = data[0].contact.head.name + ' ' + data[0].contact.head.email + ' ' + data[0].contact.head.phone
+    let rep = data[0].contact.representative.name + ' ' + data[0].contact.representative.email + ' ' + data[0].contact.representative.phone
 
-    let rep = [
-        data[0].contact.representative.name,
-        data[0].contact.representative.email,
-        data[0].contact.representative.phone
-    ]
-
-    await sendMultipleMessages(sender, head)
-    await sendMultipleMessages(sender, rep)
+    await sendTextMessage(sender, head)
+    await sendTextMessage(sender, rep)
 
 }
