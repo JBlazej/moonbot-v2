4d1a49e69fa39dfcec0ce50a9b535379beddc528
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-01-13 11:20:16 +0100

    fix bug

--- src/bot/idos/index.js
+++ src/bot/idos/index.js
@@ -13,15 +13,19 @@ import { modifyUserById, getUserById } from '../../services/user'
 export function initializeIdosTable (from, to, timeTravel, dateTravel) {
     let url = `https://jizdnirady.idnes.cz/praha/spojeni/?f=${from}&t=${to}&time=${timeTravel}&date=${dateTravel}&submit=true`
     let result = []
+
+    console.log(url)
   
     return new Promise( (resolve, reject) => {
       request(url).then((html) => {
+        console.log(html)
         const parseHtml = cheerio.load(html, { decodeEntities: false, normalizeWhitespace: true })
         cheerioTableparser(parseHtml)
   
         // Parse data from Idos
         const data = parseHtml('table tbody').attr('class', 'results').first().parsetable(true, true, true)
         // 6 columns in 1 row
+        console.log(data)
 
         for ( var j = 0; j < data[2].length; j++ ){
           const parseTable = []
