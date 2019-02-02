2a1621f4e1c904b076fe01c8eebff270f7a08908
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2019-02-02 17:42:04 +0100

    debuging

--- src/bot/news/index.js
+++ src/bot/news/index.js
@@ -111,6 +111,11 @@ export async function sendQuickNews(id, message){
         await setOffset(id, 0)
         await sendTitle(id)
     } else {
+        const supportedURL = ['vse', 'ffu', 'fmv', 'fph', 'fis', 'nf', 'fm']
+        const help = supportedURL.indexOf('vse')
+        const help2 = supportedURL.indexOf(formattedNews[1])
+        console.log(help)
+        console.log(help2)
         await setOffset(id, 0)
         await setURL(id, formattedNews[1])
         
